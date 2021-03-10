a7f91cc7d52d3c234024a9fc1cce62e8dcdd92d6
aziimii <azimi.ahmad@outlook.com>
2021-03-10 14:54:00 +0300

    login init

---
+++ src/components/login.jsx
@@ -0,0 +1,13 @@
+import { ReactComponent } from "*.svg";
+import "./components/login.css";
+
+class LoginForm extends ReactComponent {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+export default LoginForm();
